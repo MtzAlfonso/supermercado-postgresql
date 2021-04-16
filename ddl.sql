d13bd4c8dce9f7ae1ca5bf47086ffd9231a40e0a
J Alfonso Martínez Baeza <mtz.alfonso96@gmail.com>
2021-04-15 19:32:32 -0500

    Creando tabla CATALOGO

--- ddl.sql
+++ ddl.sql
@@ -1,8 +1,10 @@
 --
 -- TABLE: "PROVEEDOR"
 --
+CREATE SEQUENCE id_proveedor_seq;
+
 CREATE TABLE "PROVEEDOR" (
-  id_proveedor int NOT NULL,
+  id_proveedor int NOT NULL DEFAULT nextval('id_proveedor_seq'),
   fecha_expiracion date NOT NULL,
   nombre varchar(40) NOT NULL,
   director varchar(40) NOT NULL,
@@ -11,5 +13,16 @@ CREATE TABLE "PROVEEDOR" (
   pais varchar(20) NOT NULL,
   ciudad varchar(40) NOT NULL,
   CONSTRAINT "PK_ID_PROVEEDOR" PRIMARY KEY (id_proveedor)
+);
+
+--
+-- TABLE: "CATALOGO"
+--
+CREATE SEQUENCE id_catalogo_seq;
+
+CREATE TABLE "CATALOGO" (
+  id_catalogo int NOT NULL DEFAULT nextval('id_catalogo_seq'),
+  nombre_seccion varchar(50) NOT NULL,
+  CONSTRAINT "PK_ID_CATALOGO" PRIMARY KEY (id_catalogo)
 );
 
