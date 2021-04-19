52619f02703cc407a6d525109c471986efd55243
J Alfonso Martínez Baeza <mtz.alfonso96@gmail.com>
2021-04-19 01:40:29 -0500

    Se agrega columna calculada de bono de gerente

--- ddl.sql
+++ ddl.sql
@@ -66,6 +66,7 @@ CREATE TABLE "EMPLEADO" (
 CREATE TABLE "GERENTE" (
   id_empleado int NOT NULL,
   antiguedad int NOT NULL,
+  bono numeric(10, 2) GENERATED ALWAYS AS ((antiguedad * 1000) + 1500) STORED,
   CONSTRAINT "PK_CHILD_GERENTE" PRIMARY KEY (id_empleado)
 );
 
