658d294b2791e14b41f7cfd436b625a0070c8d28
J Alfonso Martínez Baeza <mtz.alfonso96@gmail.com>
2021-05-25 22:42:10 -0500

    Insertando datos en la tabla TARJETA

--- dml_create.sql
+++ dml_create.sql
@@ -1,3 +1,18 @@
+/*
+  -- CAJERO
+  -- CATALOGO
+  -- COMPRA
+  -- EMPLEADO
+  -- GERENTE
+  -- HISTORICO_PROD
+  -- MEMBRESIA
+  -- PRODUCTO
+  --// PROVEEDOR
+  -- STATUS
+  -- SURTE_CATALOGO
+  --// TARJETA
+*/
+
 --
 -- Insert into PROVEEDOR
 --
@@ -10,3 +25,14 @@ VALUES
 ('2023-03-14', 'Fud', NULL, 'Red Richards', '2023-12-12', 'Mexico', 'CDMX', 'Calle 4 Col. Colonia 5'),
 ('2025-02-19', 'San Rafael', 'Ryan Raynolds', 'Wade Wilson', '2025-10-24', 'Mexico', 'CDMX', 'Calle 5 Col. Colonia 5');
 
+--
+-- Insert into PROVEEDOR
+--
+INSERT INTO "TARJETA"
+(num_tarjeta, fecha_expiracion, ccv)
+VALUES
+(5596374388258012, '2022-09-01','170'),
+(5431956871808425, '2027-07-01','973'),
+(5362254023285509, '2023-11-01','577'),
+(5516155418807093, '2026-04-01','184'),
+(5395719140489078, '2024-03-01','984');
