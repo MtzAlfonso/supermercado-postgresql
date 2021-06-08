5a4039db5388ec2bf7ba8ebebf0e39745b2b3360
J Alfonso Martínez Baeza <mtz.alfonso96@gmail.com>
2021-06-08 03:13:11 -0500

    Actualizando datos de la tabla CAJERO

---
+++ dml_update.sql
@@ -0,0 +1,20 @@
+/*
+  --// CAJERO
+  -- CATALOGO
+  -- COMPRA
+  -- EMPLEADO
+  -- GERENTE
+  -- HISTORICO_PROD
+  -- MEMBRESIA
+  -- PRODUCTO
+  -- PROVEEDOR
+  -- STATUS
+  -- SURTE_CATALOGO
+  -- TARJETA
+*/
+
+---
+--- Update CAJERO
+---
+UPDATE "CAJERO" SET bachillerato = true WHERE id_empleado = 3;
+UPDATE "CAJERO" SET bachillerato = true WHERE id_empleado = 7;
